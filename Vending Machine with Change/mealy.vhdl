44a559162c2d1be5400a5d12f47e9cc300e9b7af
Ilyas Hamadouche <ilyas.match@gmail.com>
2015-11-03 21:36:47 -0800

    Vending Machine bugs fixed

--- Vending Machine with Change/mealy.vhdl
+++ Vending Machine with Change/mealy.vhdl
@@ -1,24 +1,24 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity mealy is
+entity test is
 	port(N,D,reset,clk: 	in std_logic;   --N=Naickle, D=Dim
 	     R,C:    					out std_logic);  --R=Release candy, C=Change
-end mealy;
+end test;
 
-architecture arch of mealy is
-type state_type is (0,5,10,15);
+architecture arch of test is
+type state_type is (S0,S5,S10,S15);
 signal PS: state_type;
 begin
 	process(reset, clk)
 	begin
-		if reset='1' then PS<=0;
+		if reset='1' then PS<=S0;
 		elsif(clk'event and clk='1') then
 			case PS is
-				when 0  => if(D='1') then PS<=10; elsif (N='1') PS<=5;
-				when 5  => if(D='1') then PS<=15; elsif (N='1') PS<=10;
-				when 10 => if(D='1') then PS<=0;  elsif (N='1') PS<=15;
-				when 15 => if(D='1') then PS<=0;  elsif (N='1') PS<=0;
+				when S0  => if(D='1') then PS<=S10; elsif (N='1') then PS<=S5; end if;
+				when S5  => if(D='1') then PS<=S15; elsif (N='1') then PS<=S10;end if;
+				when S10 => if(D='1') then PS<=S0;  elsif (N='1') then PS<=S15;end if;
+				when S15 => if(D='1') then PS<=S0;  elsif (N='1') then PS<=S0;end if;
 			end case;
 		end if;
 	end process;
@@ -26,9 +26,9 @@ begin
 	process(PS,N,D)
 	begin
 		case PS is
-			when 10 => if(D='1') then R<='1'; C<='0';
-      when 15 => if(D='1') then R<='1'; C<='1';
-      when 15 => if(N='1') then R<='1'; C<='0';
+			when S10 => if(D='1') then R<='1'; C<='0'; else R<='0'; C<='0'; end if;
+			when S15 => if(D='1') then R<='1'; C<='1'; elsif (N='1') then R<='1'; C<='0'; else R<='0'; C<='0'; end if;
+			when others => R<='0'; C<='0';
 		end case;
 	end process;
 end arch;
